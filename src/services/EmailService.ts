de7fa56e3548aa687eb40f9b97f0de62eb94a2d3
vamshiAtSecurelogic <Vamshi.Krishna@securelogicgroup.co>
2025-12-05 11:30:22 +0530

    fix: image bug

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,10 +1,10 @@
 import nodemailer, { Transporter } from "nodemailer";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -45,16 +45,16 @@ class EmailService {
       subject,
       text,
       html,
-      attachments: [
-        {
-          filename: "secure_logic_logo.png",
-          path: path.join(
-            __dirname,
-            "../emailTemplates/assets/secure_logic_logo.png",
-          ),
-          cid: "securelogic_logo",
-        },
-      ],
+      // attachments: [
+      //   {
+      //     filename: "secure_logic_logo.png",
+      //     path: path.join(
+      //       __dirname,
+      //       "../emailTemplates/assets/secure_logic_logo.png",
+      //     ),
+      //     cid: "securelogic_logo",
+      //   },
+      // ],
     };
 
     try {
